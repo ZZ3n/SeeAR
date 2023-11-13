4b75438fa4782969c2234421e308a878b0788964
박총명 <qkrri56@naver.com>
2023-11-13 18:46:48 +0900

    [ENH] Ui controller
    Dropdown UI controller
    option1 : Disapear object
    option2 : blue color
    option3 : green color

---
+++ Assets/ColorChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  // ⭐⭐
+
+public class ColorChange : MonoBehaviour
+{
+    public void ChangeColor(int index)
+    {
+        Color color = Color.white;
+
+        switch (index)
+        {
+            case 0:
+                gameObject.SetActive(false);
+                break;
+            case 1:
+                color = Color.blue;
+                break;
+            case 2:
+                color = Color.green;
+                break;
+            case 3:
+                color = Color.white;
+                break;
+        }
+
+        GetComponent<Renderer>().material.color = color;
+    }
+}
