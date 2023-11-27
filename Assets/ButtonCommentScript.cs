e3a6e1b69736ab519dfee4c22690189bae1e679f
박총명 <qkrri56@naver.com>
2023-11-27 21:58:50 +0900

    [ENH] 댓글 리스트 ui 추가

--- Assets/ButtonCommentScript.cs
+++ Assets/ButtonCommentScript.cs
@@ -6,6 +6,7 @@ public class ButtonCommentScript : MonoBehaviour
 {
     public GameObject ButtonEnterComment;
     public GameObject InputComment;
+    public GameObject ScrollInput;
     public bool isClicked;
 
     // Start is called before the first frame update
@@ -13,9 +14,11 @@ public class ButtonCommentScript : MonoBehaviour
     {
         ButtonEnterComment = GameObject.Find("ButtonEnterComment");
         InputComment = GameObject.Find("InputComment");
+        ScrollInput = GameObject.Find("ScrollInput");
         isClicked = false;
         ButtonEnterComment.SetActive(false);
         InputComment.SetActive(false);
+        ScrollInput.SetActive(false);
     }
 
     public void BtnClick()
@@ -25,11 +28,13 @@ public class ButtonCommentScript : MonoBehaviour
         {
             ButtonEnterComment.SetActive(true);
             InputComment.SetActive(true);
+            ScrollInput.SetActive(true);
         }
         else
         {
             ButtonEnterComment.SetActive(false);
             InputComment.SetActive(false);
+            ScrollInput.SetActive(false);
         }
 
     }
