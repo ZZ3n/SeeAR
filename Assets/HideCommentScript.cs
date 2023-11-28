5df4830a659fa6efc6fd73fc3c51a59a23dde3db
박총명 <qkrri56@naver.com>
2023-11-28 11:04:03 +0900

    [ENH] hide button 수정

--- Assets/HideCommentScript.cs
+++ Assets/HideCommentScript.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonHideComment : MonoBehaviour
+public class HideCommentScript : MonoBehaviour
 {
     public GameObject ButtonEnterComment;
     public GameObject InputComment;
     public GameObject ScrollInput;
+    public GameObject ButtonHideComment;
     // Start is called before the first frame update
     void Start()
     {
         ButtonEnterComment = GameObject.Find("ButtonEnterComment");
         InputComment = GameObject.Find("InputComment");
         ScrollInput = GameObject.Find("ScrollInput");
+        ButtonHideComment = GameObject.Find("ButtonHideComment");
     }
 
     // Update is called once per frame
@@ -21,5 +23,6 @@ public class ButtonHideComment : MonoBehaviour
         ButtonEnterComment.SetActive(false);
         InputComment.SetActive(false);
         ScrollInput.SetActive(false);
+        ButtonHideComment.SetActive(false);
     }
 }
