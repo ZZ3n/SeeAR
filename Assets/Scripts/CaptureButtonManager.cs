2c60df29a3c770ae476185cb4f19d218d0fc0097
ZZ3n <ndsrhkd@naver.com>
2023-11-30 21:40:59 +0900

    catprue rebuild

--- Assets/Scripts/CaptureButtonManager.cs
+++ Assets/Scripts/CaptureButtonManager.cs
@@ -16,13 +16,50 @@ public class CaptureButtonManager : MonoBehaviour
 
     public void SaveCameraView(Camera cam)
     {
+        if (Application.isEditor)
+        {
+            CaptureInEditor(cam);
+            return;
+        }
+
+        CaptureInFlight(cam);
+    }
+
+    void CaptureInFlight(Camera cam)
+    {
+        string fileName = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
+        byte[] byteArray = MakeScreenShot(cam);
+        NativeGallery.SaveImageToGallery(byteArray, "album", fileName,
+            (success, s) =>
+            {
+                if (success)
+                {
+                    Debug.Log("SCREENSHOT! Path : " + s);
+                }
+                else
+                {
+                    Debug.Log("Failed! Path : " + s);
+                }
+            }
+        );
+    }
+
+    void CaptureInEditor(Camera cam)
+    {
         if (!Directory.Exists(path))
         {
             Directory.CreateDirectory(path);
         }
 
         string fileName = path + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
+        byte[] byteArray = MakeScreenShot(cam);
 
+        File.WriteAllBytes(fileName, byteArray);
+        Debug.Log("SCREENSHOT! Path : " + path);
+    }
+
+    byte[] MakeScreenShot(Camera cam)
+    {
         RenderTexture screenTexture = new RenderTexture(Screen.width, Screen.height, 16);
         RenderTexture temp = cam.targetTexture;
         cam.targetTexture = screenTexture;
@@ -31,9 +68,8 @@ public class CaptureButtonManager : MonoBehaviour
         Texture2D renderedTexture = new Texture2D(Screen.width, Screen.height);
         renderedTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
         RenderTexture.active = null;
-        byte[] byteArray = renderedTexture.EncodeToPNG();
-        File.WriteAllBytes(fileName, byteArray);
-        Debug.Log("SCREENSHOT! Path : " + path);
         cam.targetTexture = temp;
+
+        return renderedTexture.EncodeToPNG();
     }
 }
